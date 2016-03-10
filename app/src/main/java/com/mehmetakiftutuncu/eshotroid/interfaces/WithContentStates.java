846a6672691fa942b42f954c70bedf6b995d1541
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2016-03-10 19:12:18 +0200

    Initial commit of something new and exciting! :)

--- app/src/main/java/com/mehmetakiftutuncu/eshotroid/interfaces/WithContentStates.java
+++
@@ -1,7 +0,0 @@
-package com.mehmetakiftutuncu.eshotroid.interfaces;
-
-import com.mehmetakiftutuncu.eshotroid.models.ContentStates;
-
-public interface WithContentStates {
-    public void changeStateTo(ContentStates newState);
-}
