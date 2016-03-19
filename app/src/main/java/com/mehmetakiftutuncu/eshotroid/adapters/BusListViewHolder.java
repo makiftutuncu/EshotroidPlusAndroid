9b01c90df2c2d998a900ab085859ec7ca9859079
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2016-03-19 16:41:14 +0200

    Started using TextDrawable for bus id drawables

--- app/src/main/java/com/mehmetakiftutuncu/eshotroid/adapters/BusListViewHolder.java
+++ app/src/main/java/com/mehmetakiftutuncu/eshotroid/adapters/BusListViewHolder.java
@@ -1,16 +1,19 @@
 package com.mehmetakiftutuncu.eshotroid.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.akashandroid90.imageletter.MaterialLetterIcon;
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.mehmetakiftutuncu.eshotroid.R;
 import com.mehmetakiftutuncu.eshotroid.models.Bus;
 
 public class BusListViewHolder extends RecyclerView.ViewHolder {
     private View mLayout;
-    private MaterialLetterIcon mMaterialLetterIconId;
+    private ImageView mImageViewId;
     private TextView mTextViewDeparture;
     private TextView mTextViewArrival;
 
@@ -19,17 +22,25 @@ public class BusListViewHolder extends RecyclerView.ViewHolder {
     public BusListViewHolder(View layout, BusListAdapter.OnBusSelectedListener onBusSelectedListener) {
         super(layout);
 
-        mLayout               = layout;
-        mMaterialLetterIconId = (MaterialLetterIcon) layout.findViewById(R.id.item_bus_list_id);
-        mTextViewDeparture    = (TextView) layout.findViewById(R.id.item_bus_list_departure);
-        mTextViewArrival      = (TextView) layout.findViewById(R.id.item_bus_list_arrival);
+        mLayout            = layout;
+        mImageViewId       = (ImageView) layout.findViewById(R.id.item_bus_list_id);
+        mTextViewDeparture = (TextView) layout.findViewById(R.id.item_bus_list_departure);
+        mTextViewArrival   = (TextView) layout.findViewById(R.id.item_bus_list_arrival);
 
         mOnBusSelectedListener = onBusSelectedListener;
     }
 
     public void setForBus(final Bus bus) {
-        mMaterialLetterIconId.setShapeColor(bus.getColor());
-        mMaterialLetterIconId.setLetter(String.valueOf(bus.id));
+        String busId = String.valueOf(bus.id);
+        TextDrawable drawable = TextDrawable
+                .builder()
+                .beginConfig()
+                .textColor(Color.WHITE)
+                .bold()
+                .endConfig()
+                .buildRound(busId, ColorGenerator.MATERIAL.getColor(busId));
+
+        mImageViewId.setImageDrawable(drawable);
         mTextViewDeparture.setText(bus.departure);
         mTextViewArrival.setText(bus.arrival);
 
